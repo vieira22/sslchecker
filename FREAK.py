dcacabbaa184f21765c02cc066b2417f7c1cf1eb
Renato Vieira <rvrs20@gmail.com>
2018-05-14 12:22:21 +0100

    Update FREAK.py

--- FREAK.py
+++ FREAK.py
@@ -1,7 +1,17 @@
 """
-#Author: Renato Vieira
-
-
+#Author: Renato Vieira - Version 2.0
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE  See the
+# GNU General Public License for more details. http://www.gnu.org/licenses/.
+Requirements:
+Python 3.4.0 - Python.org
+TLSFuzzer - https://github.com/tomato42/tlsfuzzer
+tlslite-ng - https://github.com/tomato42/tlslite-ng
+ECDSA - https://github.com/warner/python-ecdsa
+More information at:
+https://sslvulnerabilitychecker.com
+https://github.com/vieira22/SSLChecker
 """
 
 import shlex
